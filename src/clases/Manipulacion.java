0129b7f4d80a45e64a37485b87fbdbb6b5a4823b
KingGnomo115 <kevinjec115@gmail.com>
2020-11-30 15:52:50 -0600

    Ambiente creado para implementar la bd

---
+++ src/clases/Manipulacion.java
@@ -0,0 +1,16 @@
+package clases;
+
+import clases.Tipo_Usuario;
+import java.sql.Connection;
+import java.util.ArrayList;
+import poo.bd.Conexion;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Manipulacion
+  {
+
+    
+  }
